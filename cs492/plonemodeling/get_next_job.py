70fa29985104284c0528946ed8580eabdcff074a
Darkhan Kubigenov <darkhanu@gmail.com>
2014-02-02 23:57:13 -0600

    gave unrestricted access to get_next_job service

--- cs492/plonemodeling/get_next_job.py
+++ cs492/plonemodeling/get_next_job.py
@@ -2,15 +2,21 @@ import json
 from urlparse import parse_qs
 
 from Products.CMFCore.utils import getToolByName
-from Acquisition import aq_parent, aq_inner
-from zope.component import getMultiAdapter
+from Acquisition import aq_inner
 
-from cs492.plonemodeling.job import IJob
-from Products.CMFCore.utils import getToolByName
-from DateTime import DateTime
+
+
+# imports for unrestricted user
+from AccessControl import getSecurityManager
+from AccessControl.SecurityManagement import newSecurityManager, setSecurityManager
+from AccessControl.User import UnrestrictedUser as BaseUnrestrictedUser
 
 ## logging for demo
 import logging
+
+class UnrestrictedUser(BaseUnrestrictedUser):
+    def getId(self):
+        return self.getUserName()
 
 class GetNextJob(object):
     def __init__(self, context, request):
@@ -39,22 +45,53 @@ class GetNextJob(object):
         if not 'hash' in parse_result:
             return "{'response': 'NOTOK', 'reason': 'noHash'}"
         
-        context = aq_inner(self.context)
-        catalog = getToolByName(context, 'portal_catalog')
-        
-        hashValue = str(parse_result['hash'][0])
-        vms = catalog.searchResults(portal_type='cs492.plonemodeling.virtualmachine')
-        for vm in vms:
-            if vm.getObject().monitorString == hashValue:
-                jobs = catalog.searchResults(portal_type='cs492.plonemodeling.job')
-                vm_job = 0
-                for job in jobs:
-                    if (getToolByName(job.getObject(), 'virtualMachine').to_object == vm.getObject()) and (job.getObject().job_status == "Running"):
-                        job.getObject().job_status = "Finished"
-                    if (getToolByName(job.getObject(), 'virtualMachine').to_object == vm.getObject()) and (job.getObject().job_status == "Queued") and ((not vm_job) or vm_job.modified.greaterThan(job.modified)):
-                        vm_job = job
-                if vm_job:
-                    vm_job.getObject().job_status = "Running"
-                    return json.dumps({ 'response': 'OK', 'start_string': vm_job.getObject().startString  })
-		
-        return json.dumps({'response': 'NOTOK', 'reason': 'invalidHash', 'hash': hashValue })		    	
+        sm = getSecurityManager()
+
+
+        try:
+            try:
+                # Clone the current user and assign a new role.
+                # Note that the username (getId()) is left in exception
+                # tracebacks in the error_log,
+                # so it is an important thing to store.
+                tmp_user = UnrestrictedUser(
+                    sm.getUser().getId(), '', ['Manager'], ''
+                    )
+
+                # Wrap the user in the acquisition context of the portal
+                portal = self.context.portal_url.getPortalObject()
+                tmp_user = tmp_user.__of__(portal.acl_users)
+                newSecurityManager(None, tmp_user)
+
+
+                context = aq_inner(self.context)
+                catalog = getToolByName(context, 'portal_catalog')
+                
+                hashValue = str(parse_result['hash'][0])
+                vms = catalog.searchResults(portal_type='cs492.plonemodeling.virtualmachine')
+                for vm in vms:
+                    if vm.getObject().monitorString == hashValue:
+                        jobs = catalog.searchResults(portal_type='cs492.plonemodeling.job')
+                        vm_job = 0
+                        for job in jobs:
+                            if (getToolByName(job.getObject(), 'virtualMachine').to_object == vm.getObject()) and (job.getObject().job_status == "Running"):
+                                job.getObject().job_status = "Finished"
+                            if (getToolByName(job.getObject(), 'virtualMachine').to_object == vm.getObject()) and (job.getObject().job_status == "Queued") and ((not vm_job) or vm_job.modified.greaterThan(job.modified)):
+                                vm_job = job
+                        if vm_job:
+                            vm_job.getObject().job_status = "Running"
+                            return json.dumps({ 'response': 'OK', 'start_string': vm_job.getObject().startString  })
+                
+                return json.dumps({'response': 'NOTOK', 'reason': 'invalidHash', 'hash': hashValue })
+
+
+            except:
+                # If special exception handlers are needed, run them here
+                raise
+        finally:
+            # Restore the old security manager
+            setSecurityManager(sm)
+
+
+
+
