2c8d1c606ae66cf1cc3c7d571397b15b5b0d3c26
Darkhan Kubigenov <darkhanu@gmail.com>
2014-02-03 00:11:25 -0600

    changing tabs to spaces in virtual_machine.py, removing unused imports

--- cs492/plonemodeling/virtual_machine.py
+++ cs492/plonemodeling/virtual_machine.py
@@ -1,16 +1,9 @@
 from five import grok
 
-from z3c.form import group, field
 from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from plone.dexterity.content import Container
-from plone.directives import dexterity, form
-from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.directives import form
 from plone.namedfile.interfaces import IImageScaleTraversable
 
 from zope.lifecycleevent.interfaces import IObjectAddedEvent
@@ -64,7 +57,7 @@ class VirtualMachine(Container):
 
     # Add your class methods and properties here
     def getTitle(self):
-	return self.title
+        return self.title
 
 
 # View class
