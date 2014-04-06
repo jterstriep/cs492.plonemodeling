240abe7f48fc0e2657632490ddbe5d95905a6729
zheXiLiu <zhexiliu19@gmail.com>
2014-04-05 22:06:47 -0500

    reload after receiving json response

--- cs492/plonemodeling/resources/job.js
+++ cs492/plonemodeling/resources/job.js
@@ -2,8 +2,9 @@ function changeJobStatus(){
 	var jobName = document.getElementById("jobTitle").innerHTML;
 
 	$.getJSON(jobName + "/change_jobstatus", function(json) {
-
+		document.getElementById("job_status").innerHTML = json.response;
+		location.reload();
 	})	
-	location.reload();
+	
 }
 
