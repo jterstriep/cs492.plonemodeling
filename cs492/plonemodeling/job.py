323724ef3417a0c1f25e06026700a5131446dbb6
Darkhan Kubigenov <darkhanu@gmail.com>
2014-04-05 23:52:07 -0500

    beautifying job.py

--- cs492/plonemodeling/job.py
+++ cs492/plonemodeling/job.py
@@ -3,7 +3,7 @@ from five import grok
 from zope import schema
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
-from plone.dexterity.content import Item 
+from plone.dexterity.content import Item
 from plone.namedfile.interfaces import IImageScaleTraversable
 
 from z3c.relationfield.schema import RelationChoice
@@ -16,12 +16,10 @@ from Products.CMFCore.utils import getToolByName
 from plone.supermodel import model
 from cs492.plonemodeling import MessageFactory as _
 
-import socket, string, random
+import string
+import random
 
 from cs492.plonemodeling.virtual_machine import IVirtualMachine
-import urllib
-import boto.ec2
-import time
 import json
 from datetime import datetime
 
@@ -37,8 +35,9 @@ job_status_list = SimpleVocabulary(
     )
 
 AUTH_TOKEN_LENGTH = 10
-# Interface class; used to define content-type schema.
 
+
+# Interface class; used to define content-type schema.
 class IJob(model.Schema, IImageScaleTraversable):
     """
     Job which needs to be run on scientific model
@@ -51,32 +50,32 @@ class IJob(model.Schema, IImageScaleTraversable):
 
     # form.model("models/job.xml")
     startString = schema.TextLine(
-            title=_(u"command used to start the model")
+        title=_(u"command used to start the model")
     )
-    
+
     job_status = schema.Choice(
-            title=_(u"Job Status"),
-            vocabulary=job_status_list,
-            required=False,
-            default=job_status_list.getTerm(u'Pending').value
+        title=_(u"Job Status"),
+        vocabulary=job_status_list,
+        required=False,
+        default=job_status_list.getTerm(u'Pending').value
     )
-        
+
     virtualMachine = RelationChoice(
-            title=_(u"Virtual machine"),
-            source=ObjPathSourceBinder(object_provides=IVirtualMachine.__identifier__),
-            required=True,
+        title=_(u"Virtual machine"),
+        source=ObjPathSourceBinder(object_provides=IVirtualMachine.__identifier__),
+        required=True,
     )
+
 
 # Custom content-type class; objects created for this content type will
 # be instances of this class. Use this class to add content-type specific
 # methods and properties. Put methods that are mainly useful for rendering
 # in separate view classes.
-
 class Job(Item):
     grok.implements(IJob)
 
     # Add your class methods and properties here
- 
+
     def getTitle(self):
         return self.title
 
@@ -113,7 +112,8 @@ class Job(Item):
         return self.virtualMachine.to_object.title
 
     def getId(self):
-        return self.id;
+        return self.id
+
 
 # View class
 # The view will automatically use a similarly named template in
@@ -124,7 +124,6 @@ class Job(Item):
 # You may make this the default view focontextr content objects
 # of this type by uncommenting the grok.name line below or by
 # changing the view class name and template filename to View / view.pt.
-
 class SampleView(grok.View):
     """ sample view class """
 
@@ -133,7 +132,6 @@ class SampleView(grok.View):
 
     grok.name('view')
 
-    # Add view methods here
 
 @grok.subscribe(IJob, IObjectAddedEvent)
 def createJob(job, event):
@@ -145,8 +143,8 @@ def createJob(job, event):
     job.end = None
 
     ## create authorization token
-    job.monitorAuthToken = ''.join(random.choice(string.ascii_lowercase \
-        + string.digits) for _ in range(AUTH_TOKEN_LENGTH))
+    job.monitorAuthToken = ''.join(random.choice(string.ascii_lowercase +
+                                   string.digits) for _ in range(AUTH_TOKEN_LENGTH))
 
     ## Do not queue the job if status is not Queued
     if job.job_status != "Queued":
@@ -168,18 +166,16 @@ class changeJobStatus(grok.View):
     def render(self):
         self.request.response.setHeader('Content-type', 'application/json')
         context = aq_inner(self.context)
-	status_string = context.job_status;
+        status_string = context.job_status
 
-	if status_string == ('Pending' or 'Terminated' or 'Failed' or 'Finished'):
-	    context.job_status = 'Queued'
-	if status_string == 'Running':
-	    context.job_status = 'Terminated'
-	if status_string == 'Queued': 
-	    context.job_status = 'Pending'
+        if status_string == ('Pending' or 'Terminated' or 'Failed' or 'Finished'):
+            context.job_status = 'Queued'
+        if status_string == 'Running':
+            context.job_status = 'Terminated'
+        if status_string == 'Queued':
+            context.job_status = 'Pending'
 
         return json.dumps({'response': context.job_status})
-
-
 
 
 @grok.subscribe(IJob, IObjectModifiedEvent)
